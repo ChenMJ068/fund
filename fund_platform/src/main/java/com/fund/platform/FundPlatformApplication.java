ea3c3ac094fd0bbad67644b3cdcf0ddebe33007b
ChemMJ068 <616772117@qq.com>
2021-08-30 18:21:52 +0800

    feat:初始化

---
+++ fund_platform/src/main/java/com/fund/platform/FundPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.fund.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FundPlatformApplication.class, args);
+    }
+
+}
