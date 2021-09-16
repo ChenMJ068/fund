c4f84b965cd4c8616e12fdd2b0efe2ba683f35b8
ChemMJ068 <616772117@qq.com>
2021-09-16 17:41:51 +0800

    feat:init1

---
+++ fund_platform/fund_platform_api/fund_api_user/src/test/java/com/fund/platform/FundApiUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fund.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundApiUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
